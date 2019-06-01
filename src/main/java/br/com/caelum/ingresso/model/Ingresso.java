8c606e746d95dcab47f27e45becb28dc90b8545f
Victor Cardoso <victor_92santos@hotmail.com>
2019-06-01 16:47:01 -0300

    add api de bisca de dados

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -1,5 +1,32 @@
 package br.com.caelum.ingresso.model;
 
-public class Ingresso {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+import br.com.caelum.ingresso.model.descontos.IDesconto;
+
+public class Ingresso {
+	private Sessao sessao;
+	public Sessao getSessao() {
+		return sessao;
+	}
+
+	public void setSessao(Sessao sessao) {
+		this.sessao = sessao;
+	}
+
+	private BigDecimal preco;
+	
+	public Ingresso (Sessao sessao, IDesconto tipoDesconto) {
+		this.sessao = sessao;
+		this.preco = tipoDesconto.aplicaDescontoSobre(sessao.getPreco());
+	}
+	
+	public BigDecimal getPreco() {
+		return preco.setScale(2,RoundingMode.HALF_UP);
+	}
+	
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 }
